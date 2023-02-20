d3c0ab2ec47af615375dafce3b13e8e3bb3ae5a2
Justin Byrd <officialjbyrd@gmail.com>
2023-02-20 14:22:20 +0000

    Done

--- src/main/java/ArraysAreEqual.java
+++ src/main/java/ArraysAreEqual.java
@@ -15,6 +15,16 @@ public class ArraysAreEqual {
      * @return true if the values of a are equal to the values of b.
      */
     public boolean equal(int[] a, int[] b){
-        return false;
+        boolean someBool = true; 
+        for (int i = 0; i < a.length; i++){
+            if (a[i] != b[i]){
+                someBool = false;
+            }
+            
+
+            
+        }
+        return someBool;
+
     }
 }
